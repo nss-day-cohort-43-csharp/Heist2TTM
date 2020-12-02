c295c6b2f4e2d6d99990156642d6be2f1c4df53a
mnmccoy95 <mnmccoy95@gmail.com>
2020-12-02 10:44:55 -0600

    implemented robber classes

--- Muscle.cs
+++ Muscle.cs
@@ -1,7 +1,19 @@
+using System;
 namespace Heist2TTM
 {
     public class Muscle: IRobber
     {
-        
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public int PercentageCut { get; set; }
+        public void PerformSkill(Bank bank)
+        {
+            bank.SecurityGuardScore = bank.SecurityGuardScore - SkillLevel;
+            Console.WriteLine($"{this.Name} is killing the guards. Decreased security guard score by {this.SkillLevel}");
+            if(bank.SecurityGuardScore <= 0)
+            {
+                Console.WriteLine($"{this.Name} has straight up murdered all the guards! Yikes!");
+            }
+        }
     }
 }
