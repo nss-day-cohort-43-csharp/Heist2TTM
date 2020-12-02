6888132e2fda8ebbb839c56e334aea979e8b7aa3
Travis Pinson <travispinson@gmail.com>
2020-12-02 09:56:58 -0600

    Created Bank class

---
+++ Bank.cs
@@ -0,0 +1,29 @@
+namespace Heist2TTM
+{
+    public class Bank
+    {
+        public int CashOnHand { get; set; }
+
+        public int AlarmScore { get; set; }
+
+        public int VaultScore { get; set; }
+
+        public int SecurityGuardScore { get; set; }
+
+        public bool IsSecure
+        {
+            get
+            {
+                if (AlarmScore <= 0 && VaultScore <= 0 && SecurityGuardScore <= 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+    }
+}
